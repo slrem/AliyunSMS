2d5cde1f2fe72e69c0e22cf567412c28b5e4b471
sl <sl@xxx.com>
2017-11-29 16:51:11 +0800

    u

---
+++ sms.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+var aliyun = "http://dysmsapi.aliyuncs.com/?Signature="
+
+type result struct {
+	Message   string
+	RequestId string
+	BizId     string
+	Code      string
+}
+
+// accessKeyId:阿里云生成的accessKeyId
+// accessSecret:阿里云生成的accessSecret
+// signName:在阿里云申请的短信签名
+// phoneNumbers:发送的手机号码
+// templateCode:在阿里云申请的短信模板
+// templateParam:消息变量替换 JSON字符串 如{"code":"657893"}
+// return bizId:发送流水号
+func SendSms(accessKeyId, accessSecret, signName, phoneNumbers, templateCode, templateParam string) (bizId string, err error) {
+	m := make(map[string]string)
+	m["PhoneNumbers"] = phoneNumbers
+	m["SignName"] = signName
+	m["TemplateParam"] = templateParam
+	m["TemplateCode"] = templateCode
+	surl := getUrl("SendSms", accessKeyId, accessSecret, m)
+	r, err := http.Get(surl)
+	if err != nil {
+		return
+	}
+	defer r.Body.Close()
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+	var res result
+	err = json.Unmarshal(b, &res)
+	if err != nil {
+		return
+	}
+	if res.Code != "OK" {
+		err = errors.New(res.Message)
+		return
+	}
+	bizId = res.BizId
+	return
+}
+
+type A struct {
+	B []SmsSendDetailDTO `json:"SmsSendDetailDTO"`
+}
+type SmsSendDetailDTO struct {
+	OutId        string
+	SendDate     string
+	SendStatus   int
+	ReceiveDate  string
+	ErrCode      string
+	TemplateCode string
+	Content      string
+	PhoneNum     string
+}
+type SendStatusResp struct {
+	TotalCount        int
+	Code              string
+	Message           string
+	RequestId         string
+	SmsSendDetailDTOs A `json:"SmsSendDetailDTOs"`
+}
+
+// accessKeyId:阿里云生成的accessKeyId
+// accessSecret:阿里云生成的accessSecret
+// phoneNumbers:发送的手机号码
+// bizId:发送流水号,从调用发送接口返回值中获取
+// sendDate:短信发送日期格式yyyyMMdd,支持最近30天记录查询
+// pageSize:页大小Max=50
+// currentPage:当前页码
+func QuerySendDetails(accessKeyId, accessSecret, phoneNumber, bizId, sendDate, pageSize, currentPage string) (resp SendStatusResp, err error) {
+	m := make(map[string]string)
+	m["PhoneNumber"] = phoneNumber
+	m["BizId"] = bizId
+	m["SendDate"] = sendDate
+	m["PageSize"] = pageSize
+	m["CurrentPage"] = currentPage
+	surl := getUrl("QuerySendDetails", accessKeyId, accessSecret, m)
+	r, err := http.Get(surl)
+	if err != nil {
+		return
+	}
+	defer r.Body.Close()
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+	log.Println(string(b))
+	err = json.Unmarshal(b, &resp)
+	return
+}
+
+func getUrl(action, accessKeyId, accessSecret string, m map[string]string) (surl string) {
+	data := url.Values{}
+	data.Add("SignatureMethod", "HMAC-SHA1")
+	data.Add("SignatureNonce", randStr(36))
+	data.Add("AccessKeyId", accessKeyId)
+	data.Add("SignatureVersion", "1.0")
+	data.Add("Timestamp", getTime())
+	data.Add("Format", "JSON")
+
+	data.Add("Action", action)
+	data.Add("Version", "2017-05-25")
+	data.Add("RegionId", "cn-hangzhou")
+	for k, v := range m {
+		data.Add(k, v)
+	}
+
+	data.Del("Signature")
+	sortedQueryString := alReplace(data.Encode())
+	Signature := getSignature(accessSecret, sortedQueryString)
+	surl = aliyun + Signature + "&" + sortedQueryString
+	return
+}
+
+func getSignature(accessSecret, sortedQueryString string) (Signature string) {
+	xxx := "GET" + "&" + specialUrlEncode("/") + "&" + specialUrlEncode(sortedQueryString)
+	sign := sign(accessSecret+"&", xxx)
+	Signature = specialUrlEncode(sign)
+	return
+}
---
+++ tool.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"math/rand"
+	"net/url"
+	"strings"
+	"time"
+)
+
+func randStr(a int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyz-"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < a; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
+
+func getTime() string {
+	return time.Now().Add(-8 * time.Hour).Format("2006-01-02T15:04:05Z")
+
+}
+
+func alReplace(value string) string {
+	a := strings.Replace(value, "+", "%20", -1)
+	b := strings.Replace(a, "*", "%2A", -1)
+	return strings.Replace(b, "%7E", "~", -1)
+}
+
+func specialUrlEncode(value string) string {
+	value = url.QueryEscape(value)
+	a := strings.Replace(value, "+", "%20", -1)
+	b := strings.Replace(a, "*", "%2A", -1)
+	return strings.Replace(b, "%7E", "~", -1)
+}
+func sign(key, data string) string {
+	mac := hmac.New(sha1.New, []byte(key))
+	mac.Write([]byte(data))
+	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
+
+}
